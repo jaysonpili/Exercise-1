393990fa147ebfa939a3823d1b421be44a5af5d0
Jayson Pili <143930670+jaysonpili@users.noreply.github.com>
2024-08-31 17:37:44 +0800

    Add files via upload

---
+++ grp4_pili_ex1.py
@@ -0,0 +1,35 @@
+# Create a simple program with your fullname, course, year and section 
+fullname = "Jayson Pili"
+course = "BSCS"
+year = 3
+section = "G"
+print(f"Hello, I'm {fullname}! {course} {year}{section}")
+
+# Python Comments
+# My First simple script
+print("Jayson Pili\n")
+
+#Python Variables
+age = 23
+course = "Computer Science\n"
+print(age)
+print(course)
+
+#Python - Variable Names
+trisha = 21
+jenny = 22
+jewel = 21
+kyla = 24
+
+print (trisha)
+print (jenny)
+print (jewel)
+print (kyla)
+
+#Many Values to Multiple Variables
+jayson, travis_cat , lebron = "\npogi", "pusa", "kambing"
+print(jayson,travis_cat,lebron)
+
+#One Values to Multiple Variables
+jayson = travis_cat = travis_pusa = "\ncute na pusa"
+print (jayson, travis_cat,travis_pusa)
